0828289785b4c3bf650cfd095f31060497a061ee
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-08-23 12:45:33 +0300

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require(`express`);
+const handlebars = require(`express-handlebars`);
+
 const app = express();
+app.use(`/static`, express.static(`src/public`));
 
-app.use(`/static`, express.static(`public`));
+app.engine(`hbs`, handlebars.engine({
+    extname: `hbs`
+}));
+app.set(`view engine`, `hbs`);
+app.set(`views`, `./src/views`);
 
-app.get(`/`, (req,res)=>{
-     res.send(`Hello word`);
+
+app.get(`/`, (req,res)=> {
+     res.render(`index`);
 });
 
 app.listen(5000, ()=> console.log(`App is listening on port 5000`))

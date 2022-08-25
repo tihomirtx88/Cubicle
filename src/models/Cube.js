0703d6a056915a9072dca4ac99347cbbe94d4620
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-08-25 15:51:29 +0300

    fix validation bug

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -26,7 +26,7 @@ const cubeSchema = new mongoose.Schema({
 });
 
 cubeSchema.path(`imageUrl`).validate(function(){
-    return this.name.startsWith(`http`);
+    return this.imageUrl.startsWith(`http`);
 }, `Image url must be link !`)
 
 const Cube = mongoose.model(`Cube`, cubeSchema);

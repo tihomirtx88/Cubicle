73e7408d372ad0a42a059099484d114256163e68
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-08-25 22:08:34 +0300

    Add accesssory page

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 const express = require(`express`);
 const homeController = require(`./controlers/homeControllers`);
 const cubeController = require(`./controlers/cubeController`);
+const accessoryController = require(`./controlers/accessoryControler`);
 
 const router = express.Router();
 
 router.use(`/`, homeController);
 
 router.use(`/cube`, cubeController);
+
+router.use(`/accessory`, accessoryController);
 
 module.exports = router;
 

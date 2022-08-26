a5bf4f1cf646ff2a0eee90db8e430a84bbde0533
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-08-26 08:51:55 +0300

    Add attach accessory endpoint

--- src/controlers/cubeController.js
+++ src/controlers/cubeController.js
@@ -30,4 +30,8 @@ router.get(`/details/:id`, async(req,res)=>{
     res.render(`details`, {cube});
 })
 
+router.get(`/:cubeId/attach-accessory`, (req, res) => {
+    res.render(`accessory/attach`)
+})
+
 module.exports = router;
--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -20,10 +20,12 @@ const accessorySchema = new mongoose.Schema({
         required: true,
         maxLength: 120,
     },
-    cube: {
+    cubes: [
+        {
         type: mongoose.Types.ObjectId,
         ref: `Cube`,
-    }
+        }
+    ]
 })
 
 const Accessory = mongoose.model(`Accessory`, accessorySchema);

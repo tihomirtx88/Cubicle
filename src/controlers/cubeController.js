81672b8de5972166bc309e5719770ce45fff1654
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-08-26 09:01:16 +0300

    Add dynamic content to attach

--- src/controlers/cubeController.js
+++ src/controlers/cubeController.js
@@ -30,8 +30,9 @@ router.get(`/details/:id`, async(req,res)=>{
     res.render(`details`, {cube});
 })
 
-router.get(`/:cubeId/attach-accessory`, (req, res) => {
-    res.render(`accessory/attach`)
+router.get(`/:cubeId/attach-accessory`, async(req, res) => {
+    const cube = await cubeSeervices.getOne(req.params.cubeId).lean();
+    res.render(`accessory/attach`, {cube})
 })
 
 module.exports = router;
